e6902a164101fb46700172df1a87ed59c6a0db68
Pasquale Paudice <pasquale.paudice10@gmail.com>
2020-01-09 16:17:24 +0100

    *Feste Aggiunte ed evidenziate in rosso

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 
 
 
-    var data_scelta = '2018-1-1';
+    var numero = '';
+
+    var data_scelta = '2018-01-01';
 
     var data_base =  moment(data_scelta);
 
     var giorni_mese =  moment(data_scelta).daysInMonth();
+
+    var mese = data_base.get('month');
+    console.log(mese);
 
 
 
@@ -24,15 +29,22 @@ $(document).ready(function(){
     console.log("GIORNI DEL MESE");
     console.log(giorni_mese);
 
+    chiamataAjax();
+
 
 
         $("#right").click(function(){
 
+
+
             if ($('h1 span').text() != 'December') {
+
 
                 data_bases = data_base.add(1,'months').format("DD MM YYYY");
                 giorni_mese = data_base.daysInMonth();
+                mese = data_base.get('month');
                 aggiungiGiorni(giorni_mese);
+                chiamataAjax();
                 console.log(data_bases);
 
             }
@@ -41,16 +53,67 @@ $(document).ready(function(){
 
         $("#left").click(function(){
 
+
+
             if ($('h1 span').text() != 'January') {
 
                 data_bases = data_base.subtract(1,'months').format("DD MM YYYY");
                 giorni_mese = data_base.daysInMonth();
+                mese = data_base.get('month');
                 aggiungiGiorni(giorni_mese);
+                chiamataAjax();
                 console.log(data_bases);
 
             }
 
         });
+
+
+        function chiamataAjax(){
+
+
+        $.ajax({
+            'url':"https://flynn.boolean.careers/exercises/api/holidays?",
+            'data':{
+                'year':2018,
+                'month': mese
+            },
+            'methods':'get',
+            'success':function(data){
+
+                var feste = data.response;
+                var data_feste = '';
+                for (var i = 0; i < feste.length; i++) {
+
+                    console.log(feste[i]);
+                    data_feste = moment(feste[i].date).format('D');
+
+
+
+                    console.log(data_feste);
+
+                    $('li[data-mese="'+ data_feste +'"]').append(' - ' +feste[i].name);
+                    $('li[data-mese="'+ data_feste +'"]').css({'color':'red'});
+
+                }
+
+
+
+
+
+            },
+            'error':function(){
+                alert('errore');
+            }
+
+
+
+
+
+
+        });
+
+    };
 
 
 
@@ -74,8 +137,8 @@ $(document).ready(function(){
 
 
 
-        for (var i = 1; i <= giorniMese; i++) {
-            $('main ul').append('<li>' + i + ' ' + nome_mese +'</li>' );
+        for (var numero = 01; numero <= giorniMese; numero++) {
+            $('main ul').append('<li data-mese="'+ numero +'">' + numero + ' ' + nome_mese +'</li>' );
         }
 
     }
